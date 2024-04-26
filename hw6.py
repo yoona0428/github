9cb95ce668d0fa70c5e63764fe988db1b79828d2
yoona0428 <urinus0475@gmail.com>
2024-04-26 13:22:27 +0900

    Add Text File [hw6.py]

---
+++ hw6.py
@@ -0,0 +1,9 @@
+def display_multiplication() :
+    for i in range(1, 10) :
+        print(f'{2} X {i} = {2 *i:2d}    {3} X {i} = {3 *i:2d}    {4} X {i} = {4 *i:2d}    {5} X {i} = {5 *i:2d}')
+        
+    print()
+    for i in range(1, 10) :
+        print(f'{6} X {i} = {6 *i:2d}    {7} X {i} = {7 *i:2d}    {8} X {i} = {8 *i:2d}    {9} X {i} = {9 *i:2d}')
+
+display_multiplication()
